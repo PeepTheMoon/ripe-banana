a533707b024720c82896d3a29aa9fc4e436faeb5
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-06-25 13:56:55 -0700

    adds reviewer seed info to seed.js

--- database/seed.js
+++ database/seed.js
@@ -1,2 +1,11 @@
 const chance = require('chance').Chance();
 
+const Reviewer = require('./lib/models/Reviewer');
+
+module.exports = async({ reviewers = 5 } = {}) => {
+  await Reviewer.create([...Array(reviewers)].map(() => ({
+    name: chance.name(),
+    company: chance.company()
+  })));
+};
+
