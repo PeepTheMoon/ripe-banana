12659a0f627e285995a8158ac70192c112fdf04a
Briseida Pagador <bpagador@gmail.com>
2020-06-25 15:25:59 -0700

    creates new studio

--- __tests__/studio.test.js
+++ __tests__/studio.test.js
@@ -1,23 +1,37 @@
-const { MongoMemoryServer } = require('mongodb-memory-server');
-const mongod = new MongoMemoryServer();
-const mongoose = require('mongoose');
-const connect = require('../lib/utils/connect');
+// const { prepare } = require('../database/data-helpers');
+require('dotenv').config();
+require('../database/data-helpers');
 
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('ripe-banana routes', () => {
-  beforeAll(async() => {
-    const uri = await mongod.getUri();
-    return connect(uri);
+require('../lib/models/Studio');
+
+describe('studio routes', () => {
+  it('creates a studio', () => {
+    return request(app)
+      .post('/api/v1/studios')
+      .send({
+        name: 'Doc Studios',
+        address: {
+          city: 'Chicago',
+          state: 'IL',
+          country: 'USA'
+        }
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          name: 'Doc Studios',
+          address: {
+            _id: expect.anything(),
+            city: 'Chicago',
+            state: 'IL',
+            country: 'USA'
+          },
+          __v: 0 
+        });
+      });
   });
 
-  beforeEach(() => {
-    return mongoose.connection.dropDatabase();
-  });
-
-  afterAll(async() => {
-    await mongoose.connection.close();
-    return mongod.stop();
-  });
 });
--- database/seed.js
+++ database/seed.js
@@ -4,7 +4,7 @@ const Studio = require('../lib/models/Studio');
 
 module.exports = async({ createdStudios = 10 } = {}) => {
   await Studio.create([...Array(createdStudios)].map(() => ({
-    name: `${chance.capitalize(chance.word)} ${chance.capitalize(chance.word)}`, 
+    name: `${chance.capitalize(chance.word())} ${chance.capitalize(chance.word())}`, 
     address: {
       city: chance.city(),
       state: chance.state(),
