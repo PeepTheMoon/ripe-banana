06399911a5f2bde92007666aa6fa601e426ca696
Briseida Pagador <bpagador@gmail.com>
2020-06-25 14:43:51 -0700

    add studio model and route

--- database/seed.js
+++ database/seed.js
@@ -1,2 +1,14 @@
 const chance = require('chance').Chance();
 
+const Studio = require('../lib/models/Studio');
+
+module.exports = async({ createdStudios = 10 } = {}) => {
+  await Studio.create([...Array(createdStudios)].map(() => ({
+    name: `${chance.capitalize(chance.word)} ${chance.capitalize(chance.word)}`, 
+    address: {
+      city: chance.city(),
+      state: chance.state(),
+      country: chance.country()
+    }
+  })));
+};
