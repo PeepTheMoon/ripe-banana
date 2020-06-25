8063eb2bdbc1dc127257fabfffd004f844762516
smooto <melissasmoot@live.com>
2020-06-25 14:59:03 -0700

    Add Actor routesand tests

--- database/seed.js
+++ database/seed.js
@@ -1,11 +1,17 @@
 const chance = require('chance').Chance();
 
-const Reviewer = require('./lib/models/Reviewer');
+const Reviewer = require('../lib/models/Reviewer');
+const Actor = require('../lib/models/Actor');
 
-module.exports = async({ reviewers = 5 } = {}) => {
+module.exports = async({ reviewers = 5, actors = 5 } = {}) => {
   await Reviewer.create([...Array(reviewers)].map(() => ({
     name: chance.name(),
     company: chance.company()
+  })));
+  await Actor.create([...Array(actors)].map(() => ({
+    name: chance.name(),
+    dob: chance.date(),
+    pob: chance.city()
   })));
 };
 
