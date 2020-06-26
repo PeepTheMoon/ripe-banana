2faabc195fe751064750f6472dec8f674c9ce535
smooto <melissasmoot@live.com>
2020-06-25 17:56:03 -0700

    Add preliminary Film routes and tests sans Reviews

--- database/seed.js
+++ database/seed.js
@@ -3,23 +3,37 @@ const chance = require('chance').Chance();
 const Reviewer = require('../lib/models/Reviewer');
 const Actor = require('../lib/models/Actor');
 const Studio = require('../lib/models/Studio');
+const Film = require('../lib/models/Film');
 
-module.exports = async({ reviewers = 5, actors = 5, createdStudios = 10 } = {}) => {
+module.exports = async({ reviewers = 5, actors = 5, createdStudios = 10, films = 10 } = {}) => {
   await Reviewer.create([...Array(reviewers)].map(() => ({
     name: chance.name(),
     company: chance.company()
   })));
-  await Actor.create([...Array(actors)].map(() => ({
+  const allActors = await Actor.create([...Array(actors)].map(() => ({
     name: chance.name(),
     dob: chance.date(),
     pob: chance.city()
   })));
-  await Studio.create([...Array(createdStudios)].map(() => ({
+  const allStudios = await Studio.create([...Array(createdStudios)].map(() => ({
     name: `${chance.capitalize(chance.word())} ${chance.capitalize(chance.word())}`, 
     address: {
       city: chance.city(),
       state: chance.state(),
       country: chance.country()
     }
+  })));
+
+  await Film.create([...Array(films)].map(() => ({
+    title: chance.string(),
+    released: chance.date(),
+    studio: chance.pickone(allStudios),
+    cast: [{
+      role: chance.profession(),
+      actor: chance.pickone(allActors)
+    }, {
+      role: chance.profession(),
+      actor: chance.pickone(allActors)
+    }]
   })));
 };
